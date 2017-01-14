1f27466ccd478a7165e1cc0bf92d8d6e611171ac
gnorthrup <gnorthrup@uchicago.edu>
2017-01-14 17:38:09 -0600

    Chord transition file

---
+++ chord_transition.py
@@ -0,0 +1,40 @@
+import random
+import numpy as np 
+
+def pick_key():
+	key = random.randint(36,47)
+	maj = random.randint(0,1)
+	print (key, maj)
+	return (key, maj)
+
+def define_viable_chords(key_note, maj):
+	viable = []
+	major_steps = [0,2,4,5,9]
+	minor_steps = [0,3,5,7,8]
+	if_major = [1, 0, 0, 1, 0]
+	if_minor = [0, 1, 0, 0, 1]
+
+	if maj:
+		for i, step in enumerate(major_steps):
+			viable.append([key_note+step, if_major[i]])
+	elif not maj:
+		for i, step in enumerate(minor_steps):
+			viable.append([key_note+step, if_minor[i]])
+
+
+	print viable 
+	return viable 
+
+def chord_transition(viable_chords):
+	bins = [0,0.2,0.4,0.6,0.8,1]
+	trans_var = random.random()
+	counter = 0
+	while trans_var > bins[counter + 1]:
+		counter += 1
+	new_chord = viable_chords[counter]
+
+	return new_chord
+
+
+(key_note, maj) = pick_key()
+viable_chords = define_viable_chords(key_note, maj)
